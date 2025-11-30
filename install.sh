f38356cd790d3fe425c1f19ffccb7e9f069f56cd
notadevloper <nah@imgood.com>
2025-11-30 21:24:03 +0330

    i'm stupid(2)

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
-#!/usr/bin/env bash         
-                               #Why? cuz i can 
-sudo pacman -S base-devel git   cmake
-git clone https://aur.archlinux.org/yay.git  
-cd yay 
-makepkg -si 
-if command -v yay >/dev/null; then 
-echo ''
-else 
-echo r/changeyourpc 
-fi 
-sudo pacman -S fastfetch screenfetch uwufetch hyfetch countryfetch macchina cpufetch --noconfirm 
+#!/usr/bin/env bash
+                               #Why? cuz i can
+sudo pacman -S base-devel git cmake --noconfirm
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+if command -v yay >/dev/null; then
+  echo ''
+else
+  echo r/changeyourpc
+fi
+sudo pacman -S fastfetch screenfetch uwufetch hyfetch countryfetch macchina cpufetch --noconfirm
 
-yay -S neofetch	 zeptofetch weatherfetch  agesafetch	fetchfetch brokefetch-git binfetch-git anyfetch	zeitfetch gitfetc songfetch nanofetch nitchrevived albafetch-bin pfetch	 newfetch jellyfetch-git punfetch-bin	ramfetch wretch	bestfetch lfetch scu-bin beefetch nitch	crab-fetch rxfetch hwfetch-git freakyfetch  --noconfirm
-
+yay -S neofetch zeptofetch weatherfetch agesafetch fetchfetch brokefetch-git binfetch-git anyfetch zeitfetch gitfetc songfetch nanofetch nitchrevived albafetch-bin pfetch newfetch jellyfetch-git punfetch-bin ramfetch wretch bestfetch lfetch scu-bin beefetch nitch crab-fetch rxfetch hwfetch-git freakyfetch --noconfirm
